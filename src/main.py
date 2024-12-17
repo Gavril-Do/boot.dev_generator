f62b9bf71fd5b395c8cf27751f3ae7890278a18b
Gavril-Do <17620963+Gavril-Do@users.noreply.github.com>
2024-12-17 08:36:54 -0800

    Ch5: Website L1: Copy Static

--- src/main.py
+++ src/main.py
@@ -1,63 +1,19 @@
 from textnode import *
 import os, shutil
 
+from copystatic import *
+
+static_path = "./static"
+public_path = "./public"
+
+
 def main():
-	pass
+	print("deleting public directory...")
+	if os.path.exists(public_path):
+		shutil.rmtree(public_path)
 
-# Copy  contents of static to public
-def static_to_public(path):
-	to_print = []
-	if not os.path.exists("public"):
-		os.mkdir("public")
-		to_print.append("did not exist")
-	else:
-		to_print.append("did exist")
-		for filename in os.listdir("public"):
-			file_path = os.path.join("public", filename)
-			try:
-				if os.path.isfile(file_path):
-					os.unlink(file_path)
-				elif os.path.isdir(file_path):
-					shutil.rmtree(file_path)
-			except Exception as e:
-				return "failed to delete %s. Reason: %s" % (file_path, e)
-	to_print.append(copy_files(path))
-	return to_print
-
-def copy_files(path):
-	copied = []
-	items = os.listdir(path)
-	for item in items:
-		if item == "":
-			copied.append("continue")
-			continue
-		if os.path.isfile(os.path.join(path, item)):
-			# copied.append(os.path.join(path.replace("static", "public"), item))
-			copied.append(shutil.copy2(os.path.join(path, item), os.path.join(path.replace("static", "public"), item)))
-		else:
-			if not os.path.exists(os.path.join(path.replace("static", "public"), item)):
-				copied.append(os.mkdir(os.path.join(path.replace("static", "public"), item)))
-				# copied.append(os.path.join(path.replace("static", "public"), item))
-			copied.append(copy_files(os.path.join(path, item)))
-	return copied
-
-# path.removeprefix("/home/gavril/workspace/github.com/gavril-do/boot.dev_generator/static")
-	# dir_list = os.listdir(path)
-	# to_print = []
-	# for each in dir_list:
-	# 	if os.path.isfile(os.path.join(path, each)):
-	# 		to_print.append(f"{os.pardir}/boot.dev_generator/public", os.listdir())
-	# 		# to_print.append(shutil.copy(os.path.join(path, each), "/home/gavril/workspace/github.com/gavril-do/boot.dev_generator/public"))
-	# 		continue
-	# 	to_print.append(static_to_public(os.path.join(path, each)))
-	# return to_print
-	# shutil.copy2()
-	# if not os.path.exists("/home/gavril/workspace/github.com/gavril-do/boot.dev_generator/static/"):
-	# 	raise Exception("'Static' folder does not exist")
-	# # if not os.path.exists("/home/gavril/workspace/github.com/gavril-do/boot.dev_generator/public"):
-	# # 	os.mkdir("/home/gavril/workspace/github.com/gavril-do/boot.dev_generator/public")
-	# return os.path.realpath("/home/gavril/workspace/github.com/gavril-do/boot.dev_generator/static/")
-
+	print("copying static files to public directory...")
+	static_to_public(static_path, public_path)
 
 
 main()
--- src/test_main.py
+++ src/test_main.py
@@ -1,6 +1,6 @@
 import unittest
 from main import *
 
-class Teststatictopublic(unittest.TestCase):
-	def testpathexists(self):
-		print(static_to_public("/home/gavril/workspace/github.com/gavril-do/boot.dev_generator/static"))
+class TestMain(unittest.TestCase):
+	def testmain(self):
+		main()
